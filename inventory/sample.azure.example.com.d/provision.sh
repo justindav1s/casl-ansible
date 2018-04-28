cc751235a12a7f8b782f699f4131b9d0a3bd6911
Justin Davis <justinndavis@gmail.com>
2018-04-28 19:41:20 +0100

    azure infra support

--- inventory/sample.azure.example.com.d/provision.sh
+++ inventory/sample.azure.example.com.d/provision.sh
@@ -5,4 +5,5 @@ rm -rf ~/github/casl-ansible/playbooks/openshift/*.retry
 ansible-playbook -vvvv \
     -i inventory \
     ../../playbooks/openshift/end-to-end.yml \
-    -e@./inventory/group_vars/all.yml
+    -e@./inventory/group_vars/all.yml \
+    --extra-vars "operation=deploy"
