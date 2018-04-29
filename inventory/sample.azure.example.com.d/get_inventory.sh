0ba2d09203d8f0551bff427b905551cd9c57d820
Justin Davis <justinndavis@gmail.com>
2018-04-29 11:46:16 +0100

    azure infra support

--- inventory/sample.azure.example.com.d/get_inventory.sh
+++ inventory/sample.azure.example.com.d/get_inventory.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf ~/github/casl-ansible/playbooks/openshift/*.retry
-
-ansible-playbook -vvvv \
-    -i inventory \
-    ../../playbooks/openshift/end-to-end.yml \
-    -e@./inventory/group_vars/all.yml \
-    --extra-vars "operation=inventory"
+./inventory/azure_rm.py --list
