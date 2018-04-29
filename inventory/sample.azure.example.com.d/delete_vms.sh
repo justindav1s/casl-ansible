d2cbfa112dfe6f7a0e8ce843dae47ef352129556
Justin Davis <justinndavis@gmail.com>
2018-04-29 11:08:31 +0100

    azure infra support

---
+++ inventory/sample.azure.example.com.d/delete_vms.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf ~/github/casl-ansible/playbooks/openshift/*.retry
+
+ansible-playbook -vvvv \
+    -i inventory \
+    ../../playbooks/openshift/end-to-end.yml \
+    -e@./inventory/group_vars/all.yml \
+    --extra-vars "operation=absent_vms"
