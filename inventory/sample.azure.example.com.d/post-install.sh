eea67e512ff57df77fecb040afc85ea52ee72b81
Justin Davis <justinndavis@gmail.com>
2018-04-30 15:17:51 +0100

    azure infra support

--- inventory/sample.azure.example.com.d/post-install.sh
+++ inventory/sample.azure.example.com.d/post-install.sh
@@ -5,4 +5,5 @@ rm -rf ~/casl-ansible/playbooks/openshift/*.retry
 ansible-playbook -v \
     -i inventory \
     ../../playbooks/openshift/post-install.yml \
+    --module-path=~/casl-ansible \
     -e@./inventory/group_vars/all.yml
