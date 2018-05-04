83cf45543f19ff3d53a503f36ab853d7f54beb3a
Justin Davis <justinndavis@gmail.com>
2018-05-04 13:54:44 +0100

    azure support

---
+++ inventory/sample.azure.example.com.d/install_ocp2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DEBUG=
+
+ansible-playbook $DEBUG \
+    -f 20 \
+    -i inventory \
+    /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml \
+    -e@./inventory/group_vars/all.yml
+
+ansible-playbook $DEBUG \
+    -f 20 \
+    -i inventory \
+    /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
+    -e@./inventory/group_vars/all.yml
